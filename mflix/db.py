de35070cfe99bf929aff5359d1493521996f4a71
Mohamed Darwish <mu.darwish23@gmail.com>
2020-03-07 19:08:33 +0200

    completed ch2

--- mflix/db.py
+++ mflix/db.py
@@ -264,14 +264,30 @@ def get_movie(id):
                 "$match": {
                     "_id": ObjectId(id)
                 }
+            },
+            {
+                "$lookup": {
+                    "from": "comments",
+                    "let": {"id": "$_id"},
+                    "pipeline": [{
+                        "$match": {
+                            "$expr": {
+                                "$eq": ["$movie_id", "$$id"]
+                            }
+                        }
+                    },
+                        {"$sort": {"date": -1}}
+                    ],
+                    "as": "comments"
+                }
             }
         ]
 
         movie = db.movies.aggregate(pipeline).next()
         return movie
 
-    # TODO: Error Handling
-    # If an invalid ID is passed to `get_movie`, it should return None.
+   # TODO: Error Handling
+   # If an invalid ID is passed to `get_movie`, it should return None.
     except (StopIteration) as _:
 
         """
@@ -325,7 +341,12 @@ def add_comment(movie_id, user, comment, date):
     """
     # TODO: Create/Update Comments
     # Construct the comment document to be inserted into MongoDB.
-    comment_doc = {"some_field": "some_value"}
+    comment_doc = {"name": user.name,
+                   "email": user.email,
+                   "movie_id": ObjectId(movie_id),
+                   "text": comment,
+                   "date": date
+                   }
     return db.comments.insert_one(comment_doc)
 
 
@@ -339,8 +360,8 @@ def update_comment(comment_id, user_email, text, date):
     # Use the user_email and comment_id to select the proper comment, then
     # update the "text" and "date" of the selected comment.
     response = db.comments.update_one(
-        {"some_field": "some_value"},
-        {"$set": {"some_other_field": "some_other_value"}}
+        {"_id": ObjectId(comment_id), "email": user_email},
+        {"$set": {"text": text, "date": date}}
     )
 
     return response
@@ -361,7 +382,8 @@ def delete_comment(comment_id, user_email):
 
     # TODO: Delete Comments
     # Use the user_email and comment_id to delete the proper comment.
-    response = db.comments.delete_one({"_id": ObjectId(comment_id)})
+    response = db.comments.delete_one(
+        {"_id": ObjectId(comment_id), "email": user_email})
     return response
 
 
@@ -409,7 +431,7 @@ def add_user(name, email, hashedpw):
         # Insert a user with the "name", "email", and "password" fields.
         # TODO: Durable Writes
         # Use a more durable Write Concern for this operation.
-        db.users.insert_one({
+        db.users.with_options(write_concern=WriteConcern(w="majority")).insert_one({
             "name": name,
             "email": email,
             "password": hashedpw
@@ -505,8 +527,8 @@ def update_prefs(email, prefs):
         # TODO: User preferences
         # Use the data in "prefs" to update the user's preferences.
         response = db.users.update_one(
-            {"some_field": "some_value"},
-            {"$set": {"some_other_field": "some_other_value"}}
+            {"email": email},
+            {"$set": {"preferences": prefs}}
         )
         if response.matched_count == 0:
             return {'error': 'no user found'}
